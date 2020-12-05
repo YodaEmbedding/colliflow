7205012f8541a0ac76ddfb42df292c600fbd7f44
Mateen Ulhaq <mulhaq2005@gmail.com>
2020-12-05 03:50:56 -0800

    refactor: simplify Model, largely by extraction of non-mutating methods

--- colliflow/python/colliflow/model.py
+++ colliflow/python/colliflow/model.py
@@ -4,10 +4,12 @@ from queue import Queue
 from typing import (
     Any,
     Callable,
+    Container,
     Dict,
     Iterable,
     Iterator,
     List,
+    Optional,
     Sequence,
     Set,
     Tuple,
